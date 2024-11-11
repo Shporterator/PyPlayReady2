72d18ebcb7474f41d8780ba56195ca7881ab51b3
YOLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO <apid9755@gmail.com>
2024-11-12 02:51:43 +0800

    Create error.py

---
+++ modules/error.py
@@ -0,0 +1,11 @@
+class ERROR:
+    @staticmethod
+    def log(s: str):
+        """
+        Logs an error message and exits the program.
+        
+        Parameters:
+            s (str): The error message to log.
+        """
+        print(f"ERROR: {s}")
+        sys.exit(1)
